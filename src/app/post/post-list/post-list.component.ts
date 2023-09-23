c4ea922926512a574a4632c97166fceb6841e4c9
dylan9b <dylan9b@gmail.com>
2023-09-23 16:01:40 +0200

    Basic implementation for the list page.

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,7 +1,14 @@
-import { selectAllPosts, selectAllPostsCount } from 'src/state/post/post.selector';
+import { map, of } from 'rxjs';
+import {
+  selectAllPosts,
+  selectAllPostsCount,
+  selectStatus,
+} from 'src/state/post/post.selector';
 
 import { AppState } from 'src/state/app.state';
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { PostItemResponse } from '../_model/response/post-response.model';
 import { PostRequest } from '../_model/request/post-request.model';
 import { Store } from '@ngrx/store';
 import { postActions } from 'src/state/post/post.actions';
@@ -12,8 +19,18 @@ import { postActions } from 'src/state/post/post.actions';
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent {
-  allPosts$ = this._store.select(selectAllPosts);
+  allPosts$ = this._store.select(selectAllPosts).pipe(
+    map((response) => {
+      const dataSource = new MatTableDataSource<PostItemResponse>();
+      dataSource.data = response!;
+      return dataSource;
+    })
+  );
+
   allPostsCount$ = this._store.select(selectAllPostsCount);
+  status$ = this._store.select(selectStatus);
+
+  displayedColumns: string[] = ['id', 'title'];
 
   constructor(private _store: Store<AppState>) {
     let request = {} as PostRequest;
--- src/app/post/post-list/post-list.module.ts
+++ src/app/post/post-list/post-list.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { PostListComponent } from './post-list.component';
 
 @NgModule({
   declarations: [PostListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
   exports: [PostListComponent],
 })
 export class PostListModule {}
